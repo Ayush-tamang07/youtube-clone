2cea810d3b3ea527204e22f80fc3fec5dabac6b9
Ayush Tamang <tamangayush052@gmail.com>
2024-11-10 23:17:36 +0545

    added some icons to header, now i need to make video thumbnail and title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { IoNotificationsOutline } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { RiVideoUploadLine } from "react-icons/ri";
 
 function Header() {
   return (
-    <header className="flex items-center justify-between p-4 bg-gray-800">
+    <header className="flex items-center justify-between p-4">
       <div>
-        <img src="/logo.png" alt="youtube_logo" width={70} />
+        <img src="/logo.png" alt="youtube_logo" width={100} />
       </div>
       <div>
-        <input type="text" className="p-2 border rounded-md" placeholder="Search..." />
+        <input
+          type="text"
+          className="p-2 border rounded-md bg-[#0f0f0f] text-white"
+          placeholder="Search"
+        />
       </div>
-      <div>
-        <IoNotificationsOutline className="text-white" />
+      <div className="grid grid-cols-3 gap-4">
+        <RiVideoUploadLine className="text-white text-3xl" />
+        <IoNotificationsOutline className="text-white text-3xl" />
+        <CgProfile className="text-white text-3xl" />
       </div>
     </header>
   );
